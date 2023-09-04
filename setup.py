19771a40ea95bff4ab3f386d7856d7c2504a8cd2
Alexander <aandryukov@more.tv>
2023-09-04 20:35:00 +0500

    fastapi-utils from my repo

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     install_requires=[
         'fastapi==0.103.0',
         'uvicorn==0.23.2',
-        'fastapi-utils==0.2.1'
+        # Форкнем полезную библиотеку, т.к. отчего-то она несовместима с SQLAlchemy выше 1.4.47
+        'fastapi-utils @ git+https://github.com/flameai/fastapi-utils.git@aandryukov_fork#fastapi_utils'
     ]
 )
