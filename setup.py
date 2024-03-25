8efbaa8339bf46bcc7096522c33e34785d35758d
flameai <andryukov@gmail.com>
2024-03-25 15:22:01 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,6 @@ setup(
         'fastapi==0.103.0',
         'uvicorn==0.23.2',
         # Fork for fastapi-utils bcz not compatible with SQLAlchemy higher than 1.4.47
-        'fastapi-utils @ git+https://github.com/flameai/fastapi-utils.git@aandryukov_fork#fastapi_utils'
+        'fastapi-utils @ git+https://github.com/flameai/fastapi-utils.git#aandryukov_fork'
     ]
 )
